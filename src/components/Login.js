a83422b2844e859364408f3954421e599aa90393
Shubh <shubhsingh8767@gmail.com>
2023-12-10 13:44:51 +0530

    second-commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,21 +4,31 @@ import "./componentcss.css";
 export default function Login() {
   return (
     <>
-      <div className="bg-slate-600 h-screen">
-        <div className="flex flex-col align-baseline absolute top-[10rem] left-[33rem] Shadow w-[22rem] h-[18rem]">
-          <img className="w-[8rem]" src="download.jfif"></img>
-          <h1 className=" font-medium font-mono text-4xl ml-[4rem] pb-16">
-            Geofencing
-          </h1>
-          <label className="flex flex-col font-normal pr-2">
-            <p className="pb-2">E-mail</p> <input type="email" name="E-mail" />
+      <div className="bg-white h-screen Image_added">
+        <img
+          className="w-[16rem] absolute top-[11rem] left-[10rem]"
+          src="download.png"
+        ></img>
+
+        <h1 className=" font-medium font-serif text-4xl absolute top-[6rem] left-[38rem]">
+          Geofencing
+        </h1>
+
+        <div className="flex flex-col align-baseline absolute top-[10rem] left-[33rem] w-[22rem] h-fit Shadow bg-slate-100  ">
+          <label className="flex flex-col font-normal p-4">
+            <p className="pb-2 font-medium">E-mail</p>{" "}
+            <input type="email" placeholder="Enter your E-mail" name="E-mail" />
           </label>
           <br></br>
-          <lable className="flex flex-col font-normal pr-2 pb-2">
-            <p className="pb-2">Password</p>{" "}
-            <input type="password" name="password" />
+          <lable className="flex flex-col font-normal pt-1 pl-4">
+            <p className="pb-2 font-medium">Password</p>{" "}
+            <input
+              type="password"
+              placeholder="Enter your Password"
+              name="password"
+            />
           </lable>
-          <button className="bg-slate-700 text-lg font-semibold pt-4 pb-4 mt-8 ">
+          <button className=" text-lg font-semibold pt-4 pb-4 mt-8 bg-green-600  ">
             Log In
           </button>
         </div>
